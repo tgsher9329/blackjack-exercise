217c0b541c35d5db761d6bd2d918463cc2652f91
Tucker Sheram <tgsher9329@outlook.com>
2021-02-10 12:13:06 -0500

    updating blackjack game

--- scripts/main.js
+++ scripts/main.js
@@ -3,78 +3,83 @@ window.addEventListener('DOMContentLoaded', function() {
 
   const playerHandElement = document.querySelector("#player-hand")
   const dealerHandElement = document.querySelector("#dealer-hand")
+  const dealerPoints = document.querySelector("#dealer-points")
+  const playerPoints = document.querySelector("#player-points")
   const hitButton = document.querySelector("#hit-button")
   const dealButton = document.querySelector("#deal-button")
+  const messageBox = document.querySelector("#messages")
+  const standButton = document.querySelector("#stand-button")
 
   let dealerHand = []
   let playerHand = []
-
-
-
-
-
-  let deck = createDeck()
-  console.log(deck)
-  console.log(shuffle(deck))
+  let deck = []
 
 
   function shuffle(deck) {
-    let newDeck = []
-
-    for (let i = 0; i < 52; i++) {
-      let randInt = Math.floor(Math.random() * (deck.length))
-      console.log(randInt)
-      newDeck.push(deck[randInt])
-
-      let current = deck[i]
-      deck[i] = newDeck[i]
-      deck.splice(randInt, 1)
-
-      // deck.sort()
-      // console.log(newDeck)
-
+    for (let i = deck.length -1; i > 0; i--) {
+      let randInt = Math.floor(Math.random() * i)
+      let temp = deck[i]
+      deck[i] = deck[randInt]
+      deck[randInt] = temp
     }
-    return newDeck
+    return deck
   }
 
-  deal(createDeck(), playerHand)
-  function deal(deck, targetHand) {
 
-    deck.pop()
-    // delete deck[3]
-    // console.log(deck)
-
-    // console.log(deck)
-    // console.log(targetHand)
-    
-  }
 
   
-  dealButton.addEventListener("click", function() {
+  function deal(deck, targetHand, targetHandElement, targetPointsElement) {
     
-    console.log("we delt")
-    deal(newCard, playerHandElement)
-    deal(newCard, dealerHandElement)
-    deal(newCard, playerHandElement)
-    deal(newCard, dealerHandElement)
+    targetHand.push(deck[0])
+    newCardImage(deck, targetHandElement)
+    setPoints(targetHand, targetPointsElement)
+    deck.shift()
+    
 
+    if (checkBust(playerHand)) {
+      messageBox.innerText = "You busted"
+    }
+    if (checkBust(dealerHand)) {
+      messageBox.innerText = "The dealer busted"
+    }
+    
+  }
+
+  dealButton.addEventListener("click", function() {    
+    console.log("we delt")
+    deck = createDeck()
+    deck = shuffle(deck)
+
+    deal(deck, playerHand, playerHandElement, playerPoints)
+    deal(deck, dealerHand, dealerHandElement, dealerPoints)
+    deal(deck, playerHand, playerHandElement, playerPoints)
+    deal(deck, dealerHand, dealerHandElement, dealerPoints)
+
+    dealButton.disabled = true
   })
 
-  hitButton.addEventListener("click", function() {
 
+  hitButton.addEventListener("click", function() {
     console.log("you hit")
-    deal(newCard, playerHandElement)
+    deal(deck, playerHand, playerHandElement, playerPoints)
+    deal(deck, dealerHand, dealerHandElement, dealerPoints)
 
     // if dealer "score" is less than 17 "hit" them
+  })
+
+
+  standButton.addEventListener("click", function() {
+    let score = calculatePoints(dealerHand, dealerPoints)
+
+    if (score < 17) {
+      deal(deck, dealerHand, dealerHandElement, dealerPoints)
+    }
 
   })
 
   function createDeck() {
-    const deck = []
     const suits = ["hearts", "clubs", "diamonds", "spades"]
     const ranks = ["ace", "king", "queen", "jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
-
-
     for (let i = 0; i < suits.length; i++) {
       for (let j = 0; j < ranks.length; j++) {
         deck.push({
@@ -85,8 +90,53 @@ window.addEventListener('DOMContentLoaded', function() {
     }
     // shuffle() deck before return
     return deck
-
   }
 
 
+  function newCardImage(deck, targetHandElement) {
+    
+    // get a card
+    targetHandElement.innerHTML += (`<img src="images/${deck[0].rank}_of_${deck[0].suit}.png" alt="">`)
+  }
+
+
+  function calculatePoints(targetHand) {
+    score = 0
+    for (i = 0; i < targetHand.length; i++) {
+      if (isNaN(targetHand[i].rank)) {
+        if (targetHand[i].rank === "ace") {
+          score += 11
+        }
+        else {
+          score += 10
+        }
+      }
+      else {
+        score += parseInt(targetHand[i].rank, 10)
+      }
+    }
+    return score
+  }
+
+  function setPoints(targetHand, pointElement) {
+    pointElement.innerText = (`${calculatePoints(targetHand)}`)
+  }
+
+  function checkBust(hand) {
+
+
+    if (calculatePoints(hand) > 21) {
+      hitButton.disabled = true
+      standButton.disabled = true
+      return true
+    }
+    return false
+    
+  }
+
+  // console.log(deck[0])
+  // console.log(deck[0].suit)
+  // console.log(deck[0].rank)
+  
+  
 })
